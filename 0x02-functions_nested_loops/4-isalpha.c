751c7b582a6e6b1bc009e276e438b0892ade0dec
Fezite-fd <dadaoluwafemi42@gmail.com>
2022-10-21 16:45:45 -0700

    function loop

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,13 +8,5 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}}
-	else if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
